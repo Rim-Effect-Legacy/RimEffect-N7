ecf35d3216357cfa35e6d92df273ac8fe7cd524b
Simon Latusek <simon.latusek@gmail.com>
2021-04-14 00:52:12 +0200

    deflect chance on lmg

---
+++ Source/RimEffectN7/DeflectExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimEffectN7
+{
+    using Verse;
+
+    public class DeflectExtension : DefModExtension
+    {
+        public float deflectChance = 0f;
+
+        public bool Deflected => 
+            Rand.Chance(this.deflectChance);
+    }
+}
---
+++ Source/RimEffectN7/HarmonyPatches/AbsorbDamage_Patch.cs
@@ -0,0 +1,50 @@
+﻿namespace RimEffectN7
+{
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Reflection.Emit;
+    using HarmonyLib;
+    using JetBrains.Annotations;
+    using Mono.Cecil.Cil;
+    using Verse;
+    using Verse.Noise;
+
+    [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.PreApplyDamage))]
+    public static class AbsorbDamage_Patch
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
+        {
+            MethodInfo spawnedInfo = AccessTools.PropertyGetter(typeof(Thing), nameof(Pawn.Spawned));
+            bool       foundFirst  = false;
+
+            Label label = ilg.DefineLabel();
+
+            foreach (CodeInstruction instruction in instructions)
+            {
+                if(instruction.Calls(spawnedInfo))
+                    if (!foundFirst)
+                        foundFirst = true;
+                    else
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AbsorbDamage_Patch), nameof(Absorb)))
+                                     {
+                                         labels = instruction.ExtractLabels()
+                                     };
+                        yield return new CodeInstruction(OpCodes.Brfalse_S, label);
+                        yield return new CodeInstruction(OpCodes.Ldarg_2);
+                        yield return new CodeInstruction(OpCodes.Ldc_I4_1);
+                        yield return new CodeInstruction(OpCodes.Stind_I1);
+                        yield return new CodeInstruction(OpCodes.Ret);
+                        yield return new CodeInstruction(OpCodes.Ldarg_0) {labels = new List<Label> { label }};
+                        yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_HealthTracker), "pawn"));
+                    }
+
+                yield return instruction;
+            }
+        }
+
+        public static bool Absorb(Pawn pawn) => 
+            pawn.equipment.Primary?.def.GetModExtension<DeflectExtension>()?.Deflected ?? false;
+    }
+}
